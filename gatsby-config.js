e50571d3ba6723820c202d7a1171ef96d90621f8
thanawatgulati <thanawatgulatijames@gmail.com>
2020-10-10 02:57:22 +0700

    Move Directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `contents`,
-        path: `${__dirname}/src/contents`,
+        path: `${__dirname}/contents`,
       },
     },
     `gatsby-transformer-sharp`,
